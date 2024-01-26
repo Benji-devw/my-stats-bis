0104c8b91fa596598bd6f89ab5a66ed71aae08dd
Benji-devw <ben.nav@pm.me>
2024-01-26 14:42:45 +0100

    Update match and player routes

--- src/app/api/match/[id]/route.js
+++ src/app/api/match/[id]/route.js
@@ -2,6 +2,7 @@
 const db = require("/db/models/index");
 db.sequelize.sync();
 
+
 export async function GET(req, res) {
   const matchId = res.params.id;
 
@@ -11,7 +12,7 @@ export async function GET(req, res) {
       where: { id: matchId },
       include: {
         model: db.Players,
-        attributes: ["id", "name", "media"],
+        attributes: ["id", "name", "media", "player_average", "golden"],
         through: { attributes: [] }, // Exclude association table data
       },
     });
@@ -35,22 +36,51 @@ export async function GET(req, res) {
 
 // Add post method to create a new match
 export async function POST(req, res) {
-  console.log("----------------REQ-------------------", req);
-  const { body } = req;
-  const { match } = body;
+  try {
+    // Récupérer le corps de la requête POST
+    const requestBody = await req.json();
 
-  // try {
-  //   // Create Match
-  //   const newMatch = await db.Matches.create(match);
+    // Créer un nouveau match avec les données du corps de la requête
+    const newMatch = await db.Matches.create(requestBody);
 
-  //   // Return the new match as a JSON response with status 201
-  //   return new Response(JSON.stringify({ newMatch }), {
-  //     headers: { "Content-Type": "application/json" },
-  //     status: 201,
-  //   });
-  // } catch (error) {
-  //   return res
-  //     .status(400)
-  //     .json({ message: "Unable to connect to the database:", error });
-  // }
+    // Retourner le nouveau match en tant que réponse JSON avec le statut 201
+    return new Response(JSON.stringify({ newMatch }), {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ message: "Unable to connect to the database:", error });
+  }
+}
+
+// Add put method to update a match
+export async function PUT(req, res) {
+  try {
+    // Get the match ID from the request parameters
+    const matchId = res.params.id;
+
+    // Get the match from the database
+    const match = await db.Matches.findOne({ where: { id: matchId } });
+
+    // If the match doesn't exist, return a 404 error
+    if (!match) {
+      return res.status(404).json({ message: "match not found" });
+    }
+
+    // Get the updated data from the request body
+    const updatedData = await req.json();
+
+    // Update the match with the new data
+    const updatedMatch = await match.update(updatedData);
+
+    // Return the updated match as a JSON response with status 200
+    return new Response(JSON.stringify({ updatedMatch }), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ message: "Unable to connect to the database:", error });
+  }
 }
--- src/app/api/player/[id]/route.js
+++ src/app/api/player/[id]/route.js
@@ -20,8 +20,27 @@ export async function GET(req, res) {
       status: 200,
     });
   } catch (error) {
-    return res
-      .status(400)
-      .json({ message: "Unable to connect to the database:", error });
+    console.error(error);
+    return res.status(400).json({ message: "Unable to connect to the database:", error });
+  }
+}
+
+// Add post method to create a new player
+export async function POST(req, res) {
+  try {
+    // Récupérer le corps de la requête POST
+    const requestBody = await req.json();
+
+    // Créer un nouveau player avec les données du corps de la requête
+    const newPlayer = await db.Players.create(requestBody);
+
+    // Retourner le nouveau player en tant que réponse JSON avec le statut 201
+    return new Response(JSON.stringify({ newPlayer }), {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ message: "Unable to connect to the database:", error });
   }
 }
