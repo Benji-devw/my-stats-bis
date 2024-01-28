e0b990d1d2efa18632df084f6ced7fea803bf4e1
Benji-devw <ben.nav@pm.me>
2024-01-28 21:12:44 +0100

    fix/fetch_url_1.11

--- src/app/api/match/route.js
+++ src/app/api/match/route.js
@@ -1,6 +1,6 @@
 "use strict";
-// import db from "../../../../../db/models/index.js";
-const db = require("./db/models/index.js");
+import db from "../../../../db/models/index.js";
+// const db = require("./db/models/index.js");
 db.sequelize.sync();
 
 
