6011301d81fa6661d54a9da7381d7c367501e58a
Benji-devw <ben.nav@pm.me>
2024-02-28 13:59:32 +0100

    feat/match progress bar average

--- src/components/PlayerMatchStats.jsx
+++ src/components/PlayerMatchStats.jsx
@@ -3,7 +3,7 @@ import styles from "@styles/player_match_stats.module.css";
 import Image from "next/image";
 
 export default function PlayerMatchStats({players}) {
-  
+
   // get greaters values
   const greatGoals = players.map((player) => player.PlayersMatches.goals);
   const greatGoalsMax = Math.max(...greatGoals);
@@ -11,6 +11,8 @@ export default function PlayerMatchStats({players}) {
   const greatAssistsMax = Math.max(...greatAssists);
   const greatShoots = players.map((player) => player.PlayersMatches.shoots);
   const greatShootsMax = Math.max(...greatShoots);
+  const greatAverage = players.map((player) => player.PlayersMatches.average);
+  const greatAverageMax = Math.max(...greatAverage);
   
   return (
     <article className={`${styles.container} fadeIn`}>
@@ -27,12 +29,19 @@ export default function PlayerMatchStats({players}) {
             /></h2>
             <div className={`${styles.progress_bar_container}`}>
               
+              <div className={`${styles.progress_bar_average}`}>
+                <div
+                  style={{
+                    width: `${(player.PlayersMatches.average / greatAverageMax) * 100}%`}}
+                  className={`${styles.progress_average_bis} ${styles.progress_average}`}>
+                  <span className={styles.progress_text}>{player.PlayersMatches.average} %</span>
+                </div>
+              </div>
+              
               <div className={`${styles.progress_bar}`}>
                 <div
                   style={{
-                    width: `${(player.PlayersMatches.goals / greatGoalsMax) * 99}%`,
-                    transition: "width 1s ease-in-out"
-                  }}
+                    width: `${(player.PlayersMatches.goals / greatGoalsMax) * 99}%`}}
                   className={`${styles.progress_goals_bis} ${styles.progress_goals}`}>
                   <span className={styles.progress_text}>Buts</span>
                   <span className={styles.progress_text}>{player.PlayersMatches.goals}</span>
